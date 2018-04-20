d840e0b45789e1912c1559a2d58da1e8ca853272
Quecom Development <quecom-dev@Mac-mini-van-Quecom.local>
2018-04-20 14:39:24 +0200

    busy

--- src/app/user-success/user-success.component.ts
+++ src/app/user-success/user-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Globals } from "../providers/globals";
 
 @Component({
   selector: 'app-user-success',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+          public globals: Globals
+  ) { }
 
   ngOnInit() {
   }
