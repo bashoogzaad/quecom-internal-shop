1c65013ec69834de24245aa7f30b4b4f93de2c73
Quecom Development <quecom-dev@Mac-mini-van-Quecom.local>
2018-10-30 09:09:45 +0100

    Segway shop, build instead of dev

--- set-env.ts
+++ set-env.ts
@@ -6,8 +6,8 @@ export const environment = {
     production: true,
     version: "v3",
     apiUrl: "https://api.quecom.nl/customer",
-    apiKey: "l5CJaVRlvuWW2JwBi2fqsvyQBu1KZdW2AxPYhxW1",
-    debtorNumber: "103318"
+    apiKey: "AnOLxkRVFiiPFRc6pvLWTslZvtoAESXQBwMfD7JI",
+    debtorNumber: "103208"
 };
 `;
 
