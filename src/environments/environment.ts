153835f3405f1a952eadf649927cf28179b6f0c3
Quecom Development <quecom-dev@Mac-mini-van-Quecom.local>
2018-11-01 12:04:42 +0100

    Analytics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
     version: "v3",
     apiUrl: "https://api.quecom.nl/customer",
     apiKey: "l5CJaVRlvuWW2JwBi2fqsvyQBu1KZdW2AxPYhxW1",
-    debtorNumber: "103318"
+    debtorNumber: "103318",
+    gaid: "UA-128475948-5"
 };
